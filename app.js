32a7ec708df5692cccfbb8fdd76c39ed6b99d3f7
HuanXiang <huanxiangspace@qq.com>
2025-03-31 15:18:20 +0800

    update img

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('*', (req, res) => {
-    res.sendFile(new URL('./public/imgs/default.png', import.meta.url).pathname, {
+    res.sendFile(new URL('./public/imgs/default.webp', import.meta.url).pathname, {
         maxAge: 60 * 60 * 24 * 30
     })
 })
